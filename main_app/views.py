2c641c36c2bbc4746087921fd81f600a4bec5cc1
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-09-18 14:57:26 -0700

    [catcollector/main_app] initialize the django app and add main app

---
+++ main_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {'cats':cats})
+    
+class Cat: 
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
