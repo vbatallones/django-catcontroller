a7c06016f21d37ba1c8e18c80cbfcbd3cf0ab7cb
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-09-21 11:07:59 -0700

    [templates] add templates and new main_app folder with views, path, form, CRUD, and urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name="about"),
     path('cats/', views.cats_index, name="cats_index"),
+    path('cats/<int:TheseCats_id>/', views.cats_show, name="cats_show"),
+    path('cats/create/', views.TheseCatsCreate.as_view(), name='cats_create'),
+    path('cats/<int:pk>/update', views.TheseCatsUpdate.as_view(), name='cats_update'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from .models import TheseCats
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
+# Create your views here.(like controller actions)
+# create view
+class TheseCatsCreate(CreateView):
+    model = TheseCats
+    fields = '__all__'
+    success_url = '/cats'
 
-# Create your views here.
+# create a update view
+class TheseCatsUpdate(UpdateView):
+    model = TheseCats
+    fields = ['name', 'breed', 'description', 'age']
+    # form is like a req.body
+    # instead of using success_url we are going to use the function below
+    # to redirect us
+    #  form valid is a hook built in django
+    def form_valid(self, form): # this will allow us to catch the pk to redirect to the show page
+        self.object = form.save(commit=False) # dont post int he db until we say so
+        self.object.save()
+        return HttpResponseRedirect('/cats/'+str(self.object.pk))
 
 def index(request):
     return render(request, 'index.html')
@@ -9,18 +29,23 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
+    # get all cats from the db
+    cats = TheseCats.objects.all()
     return render(request, 'cats/index.html', {'cats':cats})
     
-class Cat: 
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+def cats_show(request, TheseCats_id):
+    cat = TheseCats.objects.get(id=TheseCats_id)
+    return render(request, 'cats/show.html', {'cat': cat})
+# class Cat: 
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+# cats = [
+#     Cat('Lolo', 'tabby', 'foul little demon', 3),
+#     Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#     Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
 
